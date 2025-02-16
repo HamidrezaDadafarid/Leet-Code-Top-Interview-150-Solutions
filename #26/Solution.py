fd012677e01c970ef3accc610191384df53dd80a
Hamidreza Dadafarid <148267814+HamidrezaDadafarid@users.noreply.github.com>
2025-02-16 22:43:24 +0330

    added question number 26 and 27

---
+++ #26/Solution.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        # Initialize pointer p to 1, which keeps track of the position
+        # where the next unique element should be placed.
+        p = 1
+
+        # Initialize last_unique_element to the first element of the array.
+        last_unique_element = nums[0]
+
+        # Iterate over the array starting from the second element.
+        for i in range(1, len(nums)):
+            if nums[i] != last_unique_element:
+                # If the current element is different from the last unique element,
+                # place it at the p-th position in the array.
+                nums[p] = nums[i]
+
+                # Update last_unique_element to the current element.
+                last_unique_element = nums[i]
+
+                # Increment the pointer p.
+                p += 1
+
+        # Return the length of the modified array, which is represented by p.
+        return p
+
+# Time Complexity: O(n)
+# The code iterates through the list once, so the time complexity is O(n), where n is the length of the array.
+
+# Space Complexity: O(1)
+# The algorithm uses only a few extra variables (p and last_unique_element), so the space complexity is O(1).
