11bfa870251017eab6d5176a55c6cbb277a686d8
Hamidreza Dadafarid <148267814+HamidrezaDadafarid@users.noreply.github.com>
2025-02-26 00:08:53 +0330

    added question number 141

---
+++ #141/Solution.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # Time Complexity: O(n)
+    # Space Complexity: O(1)
+    def hasCycle1(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow is fast:
+                return True
+
+        return False
+
+    # Time Complexity: O(n)
+    # Space Complexity: O(n)
+    def hasCycle2(self, head: Optional[ListNode]) -> bool:
+        s = set()
+
+        while head:
+            if head in s:
+                return True
+
+            s.add(head)
+            head = head.next
+
+        return False
